3fbdc2fef6a1fef768ad3b8a5b20647d6241d39a
nanakwamesafo <samuelkwamesafoboateng@gmail.com>
2023-11-16 14:38:26 +0000

    updated

--- built_in.c
+++ built_in.c
@@ -5,7 +5,7 @@ int builtIn_cmd(char **arg)
 {
 if (strcmp(arg[0], "exit") == 0)
 {
-sanfnt_print("Shell is exiting...\n");
+safnt_print("Shell is exiting...\n");
 exit(0);
 }
 else if (strcmp(arg[0], "cd") == 0)
@@ -15,5 +15,5 @@ chdir(getenv("HOME"));
 else
 chdir(arg[1]);
 }
-return(0)
+return(0);
 }
