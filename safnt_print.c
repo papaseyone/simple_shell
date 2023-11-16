d2f80ac79176759ba3beb54062b2eff92a32412c
papaseyone <charleskumahntim@gmail.com>
2023-11-16 07:47:39 -0800

    updated

--- safnt_print.c
+++ safnt_print.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * safnt_print - Prints a message to the screen
+ * @msg: Message to be printed
+ *
+ * Outputs the provided message to the screen.
+ */
+
 void safnt_print(const char *msg)
 {
 write(STDOUT_FILENO, msg, strlen(msg)
--- safnt_shell.c
+++ safnt_shell.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * main - Entry point for the shell program
+ *
+ * Runs an infinite loop to process user commands.
+ *
+ * Return: Always returns 0.
+ */
 
 int main(void)
 {
@@ -10,7 +17,7 @@ while (true)
 safnt_display_prompt();
 read_cmd(cmd, sizeof(cmd));
 exec_cmd(cmd);
-} 
+}
 
-return 0;
+return (0);
 }
